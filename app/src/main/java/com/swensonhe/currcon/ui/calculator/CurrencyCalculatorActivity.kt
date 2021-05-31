c6a85e2a9c5994892977b8b6383aa0993cac02b3
Mahmoud Amin <dr.mahmoud39@gmail.com>
2021-05-31 20:01:21 +0200

    Implement the Calculator logic

--- app/src/main/java/com/swensonhe/currcon/ui/calculator/CurrencyCalculatorActivity.kt
+++ app/src/main/java/com/swensonhe/currcon/ui/calculator/CurrencyCalculatorActivity.kt
@@ -1,10 +1,12 @@
 package com.swensonhe.currcon.ui.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.swensonhe.currcon.R
-import com.swensonhe.currcon.ui.latest_rates.LatestRatesViewModel
+import kotlinx.android.synthetic.main.activity_currency_calculator.*
 
 class CurrencyCalculatorActivity : AppCompatActivity() {
 
@@ -17,5 +19,23 @@ class CurrencyCalculatorActivity : AppCompatActivity() {
         setContentView(R.layout.activity_currency_calculator)
 
         viewModel.setCurrencyRateFromIntent(intent)
+
+        viewModel.getBaseCurrencyData().observe(this) { textViewBaseCurrency.text = it }
+        viewModel.getExchangeCurrencyData().observe(this) { textViewExchangedCurrency.text = it }
+        viewModel.getExchangeRateData().observe(this) { textViewExchangeRate.text = "$it" }
+
+        editTextBaseCurrency.addTextChangedListener(object : CurrConTextWatcher() {
+
+            override fun onTextChanged(value: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.setExchangeRateFor(value)
+            }
+        })
     }
+}
+
+abstract class CurrConTextWatcher : TextWatcher {
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+    override fun afterTextChanged(s: Editable?) {}
 }
