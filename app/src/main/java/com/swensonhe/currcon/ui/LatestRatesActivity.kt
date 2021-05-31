a0e9e3448a0c6a4078e50b6d56d7d293472bf489
Mahmoud Amin <dr.mahmoud39@gmail.com>
2021-05-31 16:03:03 +0200

    Add LatestRatesActivity

---
+++ app/src/main/java/com/swensonhe/currcon/ui/LatestRatesActivity.kt
@@ -0,0 +1,12 @@
+package com.swensonhe.currcon.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.swensonhe.currcon.R
+
+class LatestRatesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_latest_rates)
+    }
+}
